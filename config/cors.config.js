613520caab4af0f72d6db57feadd2117b629e47c
Andres Martinez <andresmartgonz@gmail.com>
2021-11-21 12:36:41 +0100

    cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,8 @@
 const cors = require('cors')
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  // origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: "https://moliver-api.herokuapp.com",
   allowedHeaders: ['Content-Type'],
   credentials: true
 })
