e70636d9605b910665be73b014084f547bb1731a
Andres Martinez <andresmartgonz@gmail.com>
2021-11-23 14:01:28 +0100

    updates

--- config/session.config.js
+++ config/session.config.js
@@ -3,7 +3,7 @@ const MongoStore = require('connect-mongo')(session)
 const mongoose = require('mongoose')
 const SESSION_MAX_AGE_SECONDS = Number(process.env.SESSION_MAX_AGE_SECONDS) || 60 * 60 * 24 * 7
 
-//LOCALHOST
+
 module.exports = session({
   secret: process.env.SESSION_SECRET || 'Moliver',
   resave: true,
@@ -19,20 +19,3 @@ module.exports = session({
     ttl: SESSION_MAX_AGE_SECONDS
   })
 })
-
-//HEROKU
-// module.exports = session({
-//   secret: process.env.SESSION_SECRET || 'Moliver',
-//   resave: true,
-//   saveUninitialized: false,
-//   cookie: {
-//     sameSite: 'none',
-//     secure: process.env.SESSION_SECURE,
-//     httpOnly: true,
-//     maxAge: SESSION_MAX_AGE_SECONDS * 1000
-//   },
-//   store: new MongoStore({
-//     mongooseConnection: mongoose.connection,
-//     ttl: SESSION_MAX_AGE_SECONDS
-//   })
-// })
