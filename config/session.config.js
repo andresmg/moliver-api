3dc81ff59e63829eebce80bb8b5fe46a6fabae40
Andres Martinez <andresmartgonz@gmail.com>
2021-11-23 13:39:19 +0100

    updates

--- config/session.config.js
+++ config/session.config.js
@@ -3,30 +3,14 @@ const MongoStore = require('connect-mongo')(session)
 const mongoose = require('mongoose')
 const SESSION_MAX_AGE_SECONDS = Number(process.env.SESSION_MAX_AGE_SECONDS) || 60 * 60 * 24 * 7
 
-
-// module.exports = session({
-//   secret: process.env.SESSION_SECRET || 'Moliver',
-//   resave: true,
-//   saveUninitialized: false,
-//   cookie: {
-//     secure: false,
-//     httpOnly: true,
-//     maxAge: SESSION_MAX_AGE_SECONDS * 1000
-//   },
-//   store: new MongoStore({
-//     mongooseConnection: mongoose.connection,
-//     ttl: SESSION_MAX_AGE_SECONDS
-//   })
-// })
-
-//HEROKU
+//LOCALHOST
 module.exports = session({
   secret: process.env.SESSION_SECRET || 'Moliver',
   resave: true,
   saveUninitialized: false,
   cookie: {
-    sameSite: 'none',
-    secure: process.env.SESSION_SECURE,
+    sameSite: process.env.SESSION_SAMESITE || true,
+    secure: process.env.SESSION_SECURE || false,
     httpOnly: true,
     maxAge: SESSION_MAX_AGE_SECONDS * 1000
   },
@@ -35,3 +19,20 @@ module.exports = session({
     ttl: SESSION_MAX_AGE_SECONDS
   })
 })
+
+//HEROKU
+// module.exports = session({
+//   secret: process.env.SESSION_SECRET || 'Moliver',
+//   resave: true,
+//   saveUninitialized: false,
+//   cookie: {
+//     sameSite: 'none',
+//     secure: process.env.SESSION_SECURE,
+//     httpOnly: true,
+//     maxAge: SESSION_MAX_AGE_SECONDS * 1000
+//   },
+//   store: new MongoStore({
+//     mongooseConnection: mongoose.connection,
+//     ttl: SESSION_MAX_AGE_SECONDS
+//   })
+// })
